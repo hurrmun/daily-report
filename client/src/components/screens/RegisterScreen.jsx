c67d286ec23f8489089b5b7c9873124e8115a42d
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-23 16:11:55 +0800

    updated register page styling

--- client/src/components/screens/RegisterScreen.jsx
+++ client/src/components/screens/RegisterScreen.jsx
@@ -62,14 +62,19 @@ const RegisterScreen = () => {
   return (
     <>
       <div className="max-w-4xl lg:max-w-7xl mx-auto pt-4 px-4 sm:pt-6 lg:px-8">
-        <div className="grid grid-cols-1 justify-items-center">
-          <form onSubmit={registerHandler}>
+        <form onSubmit={registerHandler}>
+          <div className="grid grid-cols-1 justify-items-center text-midnight">
             <h3 className="text-midnight text-4xl font-bold">Register</h3>
             {error && (
               <span className="text-white bg-brown-sugar">{error}</span>
             )}
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
+            <div className="form-group mt-2 w-80">
+              <label
+                htmlFor="username"
+                className="font-semibold text-lg text-pine-green"
+              >
+                Username
+              </label>
               <input
                 type="text"
                 required
@@ -77,10 +82,16 @@ const RegisterScreen = () => {
                 placeholder="Enter Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
+                className="border border-shamrock-green rounded-sm w-full h-8 pl-2 placeholder-opacity-50 placeholder-shamrock-green"
               />
             </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
+            <div className="form-group mt-2 w-80">
+              <label
+                htmlFor="email"
+                className="font-semibold text-lg text-pine-green"
+              >
+                Email
+              </label>
               <input
                 type="email"
                 required
@@ -88,10 +99,16 @@ const RegisterScreen = () => {
                 placeholder="Enter Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                className="border border-shamrock-green rounded-sm w-full h-8 pl-2 placeholder-opacity-50 placeholder-shamrock-green"
               />
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
+            <div className="form-group mt-2 w-80">
+              <label
+                htmlFor="password"
+                className="font-semibold text-lg text-pine-green"
+              >
+                Password
+              </label>
               <input
                 type="password"
                 required
@@ -99,10 +116,16 @@ const RegisterScreen = () => {
                 placeholder="Enter Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                className="border border-shamrock-green rounded-sm w-full h-8 pl-2 placeholder-opacity-50 placeholder-shamrock-green"
               />
             </div>
-            <div className="form-group">
-              <label htmlFor="confirmPassword">Confirm Password</label>
+            <div className="form-group mt-2 w-80">
+              <label
+                htmlFor="confirmPassword"
+                className="font-semibold text-lg text-pine-green"
+              >
+                Confirm Password
+              </label>
               <input
                 type="password"
                 required
@@ -110,19 +133,26 @@ const RegisterScreen = () => {
                 placeholder="Confirm Password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
+                className="border border-shamrock-green rounded-sm w-full h-8 pl-2 placeholder-opacity-50 placeholder-shamrock-green"
               />
             </div>
-            <button type="submit" className="">
+            <button
+              type="submit"
+              className="mt-4 bg-shamrock-green rounded-sm w-80 text-white font-lg py-2 font-bold hover:bg-brown-sugar"
+            >
               Register
             </button>
             <div className="grid justify-items-center w-80">
               <span className="text-red-600 text-sm mt-4">
                 Already have an account?
-                <Link to="/login">Login</Link>
+                <Link to="/login" className="font-bold hover:text-brown-sugar">
+                  {" "}
+                  Login
+                </Link>
               </span>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </>
   );
