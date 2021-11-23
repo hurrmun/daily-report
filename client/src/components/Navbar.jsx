318834b03ef2ab86e763f3598e106970827af0dd
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-23 17:41:08 +0800

    logout button now logs you out and private screen makes api call to get privateinfo (will be changed to homepage)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
-import { NavLink } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = (props) => {
+  const navigate = useNavigate();
   const auth = localStorage.getItem("authToken");
-  const LogOutButton = () => {};
+  const LogOutButton = (props) => {
+    return (
+      <button
+        onClick={props.logoutHandler}
+        className="mx-3 font-medium text-white hover:text-midnight"
+      >
+        Logout
+      </button>
+    );
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login", { replace: true });
+  };
+
   return (
     <>
       <nav className="bg-shamrock-green fixed w-full py-4 z-10">
-        <div className="max-w-5xl lg:max-w-7xl mx-auto">
+        <div className="max-w-5xl lg:max-w-12xl mx-auto">
           <div className="flex justify-between">
             <NavLink to="/" className="flex items-center text-white mx-3">
               <span className="text-xl font-bold">Daily Report</span>
             </NavLink>
             {props.loggedIn || auth ? (
-              <button className="mx-3 font-medium text-white hover:text-midnight">
-                Logout
-              </button>
+              <LogOutButton logoutHandler={logoutHandler} />
             ) : null}
           </div>
         </div>
